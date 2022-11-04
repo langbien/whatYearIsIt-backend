722b424cea5517235e44a0806307a4be3a68e720
Langbien <pierre.langbien@gmail.com>
2022-11-04 15:23:24 +0100

    deployBack

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+
+router.get('/date', (req, res) => {
+ const date = new Date();
+ res.json({ now: date });
+});
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
